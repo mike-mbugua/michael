f462188ff6069dc5871b65969a18d681fb75b60b
michael-mbugua <kamirimichael369@gmail.com>
2023-05-16 23:18:28 +0300

    add code to contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import contact from './../../assets/images/contact-me.svg'
+import { motion } from 'framer-motion'
+import './contact.css'
+import {
+  faInstagram,
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Contact() {
+  return (
+    <div className="contact__container">
+      <div className="leftContact">
+        <div>
+          <motion.div
+            whileHover={{
+              scale: 1,
+              transition: { duration: 1 },
+            }}
+          >
+            <img src={contact} alt="" height={200} width={'100%'} />
+          </motion.div>
+        </div>
+        {/* < className="contact__paragraph"> */}
+        <h1>Michael Kamiri Mbugua</h1>
+        <p>
+          Full Stack Developer <br />
+          I am available for freelance work. Connect with me through a phone
+          call or through my social media <br />
+          <span className="phone">
+            Phone:<span style={{ color: '#fb0700' }}>+254705197981</span>
+          </span>{' '}
+          <br />
+          <span className="phone">
+            Email:
+            <span style={{ color: '#fb0700' }}>kamirimichael369@gmail.com</span>
+          </span>
+        </p>
+
+        <div className="icons">
+          <motion.div
+            className="socials"
+            whileHover={{
+              scale: 1.3,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href="#instagram">
+              <FontAwesomeIcon
+                icon={faInstagram}
+                style={{ fontSize: '35', color: 'white' }}
+              />
+            </a>
+          </motion.div>
+          <motion.div
+            className="socials"
+            whileHover={{
+              scale: 1.3,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href="#instagram">
+              <FontAwesomeIcon
+                className="insta"
+                icon={faTwitter}
+                style={{ fontSize: '35', color: 'white' }}
+              />
+            </a>
+          </motion.div>
+          <motion.div
+            className="socials"
+            whileHover={{
+              scale: 1.3,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href="#instagram">
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                size={20}
+                style={{ fontSize: '35', color: 'white' }}
+              />
+            </a>
+          </motion.div>
+          <motion.div
+            className="socials"
+            whileHover={{
+              scale: 1.3,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <a href="#instagram">
+              <FontAwesomeIcon
+                icon={faGithub}
+                style={{ fontSize: '35', color: 'white' }}
+              />
+            </a>
+          </motion.div>
+        </div>
+      </div>
+
+      {/*Form ---------------------------------------------------------  */}
+      <div className="right__contact">
+        <form action="">
+          <div className="holder1">
+            <div className="form1">
+              <label htmlFor="name">Name</label>
+              <input type="text" placeholder="Your Name" />
+            </div>
+            <div className="form1">
+              <label htmlFor="name">Phone</label>
+              <input type="number" placeholder="Phone Number" />
+            </div>
+          </div>
+          <div className="form1">
+            <label htmlFor="name">Email</label>
+            <input type="email" placeholder="example@gmail.com" />
+          </div>
+          <br />
+          <div className="form1">
+            <label htmlFor="name">Subject</label>
+            <input type="text" placeholder="Subject" />
+          </div>
+          <br />
+          <div className="form1">
+            <label htmlFor="name">Message</label>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+        </form>
+        <button className="primary__btn">send Message</button>
+        <br />
+      </div>
+    </div>
+  )
+}
+
+export default Contact
